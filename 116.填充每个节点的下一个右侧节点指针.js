c3668389a1c143465cf8e4d411218ec41d15c974
xun <2042204285@qq.com>
2022-08-23 22:07:41 +0800

    moment

---
+++ 116.填充每个节点的下一个右侧节点指针.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=116 lang=javascript
+ *
+ * [116] 填充每个节点的下一个右侧节点指针
+ */
+
+// @lc code=start
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function (root) {
+  if (root === null) return root;
+
+  const queue = [root];
+
+  while (queue.length) {
+    let length = queue.length;
+    while (length--) {
+      const node = queue.shift();
+
+      if (length > 0) node.next = queue[0];
+
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+  }
+
+  return root;
+};
+// @lc code=end
---
+++ 637.二叉树的层平均值.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=637 lang=javascript
+ *
+ * [637] 二叉树的层平均值
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function (root) {
+  const result = [];
+  if (root === null) return result;
+
+  let queue = [root];
+
+  while (queue.length) {
+    const length = queue.length;
+
+    let sum = 0;
+    for (let i = 0; i < length; i++) {
+      const node = queue.shift();
+      sum += node.val;
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+    result.push(sum / length);
+  }
+
+  return result;
+};
+// @lc code=end
