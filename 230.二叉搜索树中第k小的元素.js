b8106c77fd19cd3f4dd94759514ecfda53a52fe8
xun <2042204285@qq.com>
2022-08-25 21:44:20 +0800

    moment

---
+++ 230.二叉搜索树中第k小的元素.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=javascript
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (root, k) {
+  let count = 0;
+  const stack = [];
+
+  while (root || stack.length) {
+    while (root) {
+      stack.push(root);
+      root = root.left;
+    }
+    root = stack.pop();
+
+    if (++count === k) return root.val;
+
+    root = root.right;
+  }
+};
+// @lc code=end
