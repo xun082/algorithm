8d84bc12e6661c367cb958338f117ee9d3285175
xun <2042204285@qq.com>
2022-09-29 23:08:42 +0800

    moment

--- 35.搜索插入位置.js
+++ 35.搜索插入位置.js
@@ -10,5 +10,25 @@
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function (nums, target) {};
+var searchInsert = function (nums, target) {
+  const length = nums.length;
+
+  let left = 0;
+  let right = length - 1;
+  let ans = length;
+
+  while (left <= right) {
+    // 取中间
+    let mid = ((right - left) >> 1) + left;
+
+    if (target <= nums[mid]) {
+      ans = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return ans;
+};
 // @lc code=end
