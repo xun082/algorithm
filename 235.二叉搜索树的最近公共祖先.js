fc9f67a9a25f06f7a5ed75b44e22491c09a9f34a
xun <2042204285@qq.com>
2022-10-02 21:50:20 +0800

    monent

---
+++ 235.二叉搜索树的最近公共祖先.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=235 lang=javascript
+ *
+ * [235] 二叉搜索树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  if (p.val < root.val && q.val < root.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else if (p.val > root.val && q.val > root.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  } else {
+    return root;
+  }
+};
+// @lc code=end
