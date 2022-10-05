c3d4bb6be0df0493eb244b112171042c9da5ad41
xun <2042204285@qq.com>
2022-10-05 09:01:38 +0800

    moment

--- 169.多数元素.js
+++ 169.多数元素.js
@@ -9,8 +9,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
+var majorityElement = function (nums) {
+  const map = new Map();
 
+  for (let i = 0; i < nums.length; i++) {
+    //   如果存在,则取出他的值,也就是count
+    // 不存在count就为0
+    let count = map.get(nums[i]) || 0;
+    count++;
+
+    //   如果count大于数组长度的一半则成立
+    if (count > nums.length / 2) return nums[i];
+
+    map.set(nums[i], count);
+  }
 };
 // @lc code=end
-
