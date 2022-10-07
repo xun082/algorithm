c9b6d6c509c914c8151b87353d3510b240bd8e3e
xun <2042204285@qq.com>
2022-10-07 11:45:21 +0800

    moment

---
+++ 231.2-的幂.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=231 lang=javascript
+ *
+ * [231] 2 的幂
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function (n) {
+  return n > 0 && (n & -n) === n;
+};
+// @lc code=end
