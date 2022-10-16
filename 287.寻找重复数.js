48c3d649c9a5d86133b1d0ae7def396c2177ea20
xun <2042204285@qq.com>
2022-10-16 20:04:35 +0800

    moment

---
+++ 287.寻找重复数.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=287 lang=javascript
+ *
+ * [287] 寻找重复数
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function (nums) {
+  let slow = 0;
+  let fast = 0;
+
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } while (slow !== fast);
+
+  slow = 0;
+
+  while (slow !== fast) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+
+  return slow;
+};
+// @lc code=end
---
+++ 350.两个数组的交集-ii.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=350 lang=javascript
+ *
+ * [350] 两个数组的交集 II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function (nums1, nums2) {
+  nums1.sort((a, b) => a - b);
+  nums2.sort((a, b) => a - b);
+
+  let i = 0;
+  let j = 0;
+  const result = [];
+
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] === nums2[j]) {
+      result.push(nums1[i]);
+      i++;
+      j++;
+    } else {
+      nums1[i] < nums2[j] ? i++ : j++;
+    }
+  }
+  return result;
+};
+// @lc code=end
