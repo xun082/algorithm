8735a89cef283b97d9a5974d760908a7ec3d05a7
xun <2042204285@qq.com>
2022-10-24 21:49:37 +0800

    moment

---
+++ 128.最长连续序列.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=128 lang=javascript
+ *
+ * [128] 最长连续序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function (nums) {
+  const set = new Set();
+  for (const num of nums) {
+    set.add(num);
+  }
+
+  let length = 0;
+
+  for (const num of set) {
+    if (!set.has(num - 1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+
+      while (set.has(currentNum + 1)) {
+        currentNum++;
+        currentStreak++;
+      }
+      length = Math.max(length, currentStreak);
+    }
+  }
+
+  return length;
+};
+// @lc code=end
