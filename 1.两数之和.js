1adcbf566539f34598567ea9bf17560cb302d336
xun <2042204285@qq.com>
2022-11-02 09:50:14 +0800

    moment

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -10,37 +10,32 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  var ten = 0;
-  var head = null,
-    tail = null;
-  while (l1 || l2) {
-    var l1Val = l1 !== null && l1.val ? l1.val : 0;
-    var l2Val = l2 !== null && l2.val ? l2.val : 0;
-    var sum = l1Val + l2Val + ten;
-    if (sum >= 10) {
-      ten = 1;
+var twoSum = function (nums, target) {
+  /**
+   * @author 暴力破解
+   */
+  // const length = nums.length;
+  // for (let i = 0; i < length; i++) {
+  //   for (let j = 0; j < length; j++) {
+  //     if (nums[i] + nums[j] === target && i !== j) {
+  //       return [i, j];
+  //     }
+  //   }
+  // }
+  /**
+   * @author 哈希表
+   */
+  const object = {};
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    // n是需要的值
+    let n = target - num;
+
+    if (num in object) {
+      return [i, object[num]];
     } else {
-      ten = 0;
-    }
-    sum = sum % 10;
-    if (!head) {
-      head = tail = new ListNode(sum);
-    } else {
-      tail.next = new ListNode(sum);
-      tail = tail.next;
-    }
-    if (l1) {
-      l1 = l1.next;
-    }
-    if (l2) {
-      l2 = l2.next;
+      object[n] = i;
     }
   }
-  if (ten) {
-    tail.next = new ListNode(ten);
-  }
-  return head;
 };
 // @lc code=end
-
