6b2a902ee7b7cf053163c8906ddee6e224410eda
xun <2042204285@qq.com>
2022-11-02 08:13:20 +0800

    moment

---
+++ 10.正则表达式匹配.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=10 lang=javascript
+ *
+ * [10] 正则表达式匹配
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function (s, p) {
+  const getIsMatch = (s, p) => {
+    // 如果传入p的长度为0,那么,必须s的长度为0才会返回true
+    if (p.length === 0) {
+      return !s.length;
+    }
+
+    // 判断第一个字符是否相等
+    let match = false;
+    if (s.length > 0 && (s[0] === p[0] || p[0] === ".")) {
+      match = true;
+    }
+
+    // p含有 *
+    if (p.length > 1 && p[1] === "*") {
+      //第一种情况：s*匹配0个字符
+      //第二种情况：s*匹配1个字符，递归下去，用来表示s*匹配多个s*
+      return getIsMatch(s, p.slice(2)) || (match && getIsMatch(s.slice(1), p));
+    } else {
+      return match && getIsMatch(s.slice(1), p.slice(1));
+    }
+  };
+
+  return getIsMatch(s, p);
+};
+// @lc code=end
