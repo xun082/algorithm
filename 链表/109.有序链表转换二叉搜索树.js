faf00f63a9dd200efd7c3cda38ddb41727f5de44
xun <2042204285@qq.com>
2022-11-06 15:59:49 +0800

    moment

---
+++ 链表/109.有序链表转换二叉搜索树.js
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=109 lang=javascript
+ *
+ * [109] 有序链表转换二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {TreeNode}
+ */
+var sortedListToBST = function (head) {
+  /**
+   * @author xun
+   * @method 递归
+   * @timeComplexity
+   * @spaceComplexity
+   */
+  // function traverse(head, tail) {
+  //   if (head === tail) return null;
+  //   let slow = (fast = head);
+  //   while (fast !== tail && fast.next !== tail) {
+  //     fast = fast.next.next;
+  //     slow = slow.next;
+  //   }
+  //   const root = new TreeNode(slow.val);
+  //   root.left = traverse(head, slow);
+  //   root.right = traverse(slow.next, tail);
+  //   return root;
+  // }
+  // return traverse(head, null);
+  /**
+   * @author xun
+   * @method  快慢指针
+   * @timeComplexity O(n)
+   * @spaceComplexity O(n)
+   */
+
+  const node = {};
+
+  if (!head) return null;
+
+  let slow = head;
+  let fast = head;
+  let last = head;
+
+  while (fast && fast.next) {
+    last = slow;
+
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  node.val = slow.val;
+
+  // 因为下面有if 判断，所以先给left置一个null，否则不满足TreeNode type
+  node.left = null;
+
+  // 递归左节点
+  if (last !== slow) {
+    last.next = null;
+    node.left = sortedListToBST(head);
+  }
+
+  // 递归又节点
+  node.right = sortedListToBST(slow.next);
+
+  return node;
+};
+// @lc code=end
