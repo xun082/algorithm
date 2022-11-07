b277249cd2c3c88474f4a8e061438267056906ab
xun <2042204285@qq.com>
2022-11-07 22:26:47 +0800

    moment

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
-/*
- * @lc app=leetcode.cn id=21 lang=javascript
- *
- * [21] 合并两个有序链表
- */
+let slow = (fast = head);
+let prevent;
+while (fast && fast.next) {
+  fast = fast.next.next;
 
-// @lc code=start
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} list1
- * @param {ListNode} list2
- * @return {ListNode}
- */
-var mergeTwoLists = function (list1, list2) {
-  if (list1 === null) return list2;
-  if (list2 === null) return list1;
+  //   翻转
+  let next = slow.next;
+  slow.next = prevent;
+  prevent = slow;
+  slow = next;
+}
+// slow在中间 奇数问题
+if (fast) {
+  slow = slow.next;
+}
 
-  if (list1.val < list2.val) {
-    list1.next = mergeTwoLists(list1.next, list2);
-    return list1;
-  } else {
-    list2.next = mergeTwoLists(list1, list2.next);
-    return list2;
+while (prevent && slow) {
+  if (prevent.val !== slow.val) {
+    return false;
   }
-};
-// @lc code=end
+  prevent = prevent.next;
+  slow = slow.next;
+}
+return true;
---
+++ 链表/234.回文链表.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=234 lang=javascript
+ *
+ * [234] 回文链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+  /**
+   * @author xun
+   * @method 快慢指针
+   * @timeComplexity O(N)
+   * @spaceComplexity O(1)
+   */
+
+  // 链表翻转
+  const reverseList = (head) => {
+    let prev = null;
+    let curr = head;
+    while (curr !== null) {
+      let nextTemp = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = nextTemp;
+    }
+    return prev;
+  };
+
+  // 找出中间的链表节点
+  const endOfFirstHalf = (head) => {
+    let fast = head;
+    let slow = head;
+    while (fast.next !== null && fast.next.next !== null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+    return slow;
+  };
+
+  if (head === null) return true;
+
+  // 找到前半部分链表的尾结点并翻转后半部分
+  const firstHalfEnd = endOfFirstHalf(head);
+  const secondHalfStart = reverseList(firstHalfEnd.next);
+
+  // 判断是否回文
+  let p1 = head;
+  let p2 = secondHalfStart;
+
+  let result = true;
+
+  while (result && p2 !== null) {
+    if (p1.val !== p2.val) return false;
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+
+  // 还原链表并返回结果
+  firstHalfEnd.next = reverseList(secondHalfStart);
+  return result;
+};
+// @lc code=end
