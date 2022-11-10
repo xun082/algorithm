4e838cd7a082b26cf385b6086dfd4c8155657b10
xun <2042204285@qq.com>
2022-11-10 20:46:32 +0800

    moment

--- 树/101.对称二叉树.js
+++ 树/101.对称二叉树.js
@@ -18,14 +18,23 @@
  * @return {boolean}
  */
 var isSymmetric = function (root) {
-  // 递归
-  const check = (p, q) => {
-    if (!p && !q) return true;
-    if (!q || !p) return false;
-
-    return p.val === q.val && check(p.left, q.right) && check(p.right, q.left);
-  };
-
-  return check(root, root);
+  /**
+   * @author xun
+   * @method 递归
+   * @timeComplexity O(N)
+   * @spaceComplexity O(N)
+   */
+  // const check = (p, q) => {
+  //   if (!p && !q) return true;
+  //   if (!p || !q) return false;
+  //   return p.val === q.val && check(p.left, q.right) && check(p.right, q.left);
+  // };
+  // return check(root, root);
+  /**
+   * @author xun
+   * @method 迭代
+   * @timeComplexity O(N)
+   * @spaceComplexity O(1)
+   */
 };
 // @lc code=end
