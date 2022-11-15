3362c26b4b1cd2aa8ff487cf9186aae6fb2d37d8
xun <2042204285@qq.com>
2022-11-16 07:05:09 +0800

    moment

--- index.js
+++ index.js
@@ -1,24 +1,80 @@
-let slow = (fast = head);
-let prevent;
-while (fast && fast.next) {
-  fast = fast.next.next;
+/*
+ * @lc app=leetcode.cn id=113 lang=javascript
+ *
+ * [113] 路径总和 II
+ */
 
-  //   翻转
-  let next = slow.next;
-  slow.next = prevent;
-  prevent = slow;
-  slow = next;
-}
-// slow在中间 奇数问题
-if (fast) {
-  slow = slow.next;
-}
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+var pathSum = function (root, targetSum) {
+  // 深度优先
+  //   const arr = [];
+  //   const path = [];
+  //   dfs(root, path, arr, targetSum);
+  //   function dfs(root, path, arr, sum) {
+  //     if (!root) return;
+  //     path.push(root.val);
+  //     if (root.val == sum && root.left == null && root.right == null) {
+  // 满足要求且该节点为叶子节点
+  //       arr.push([...path]);
+  //     }
+  //     dfs(root.left, path, arr, sum - root.val);
+  //     dfs(root.right, path, arr, sum - root.val);
+  //     path.pop();
+  //   }
+  //   return arr;
 
-while (prevent && slow) {
-  if (prevent.val !== slow.val) {
-    return false;
+  const result = [];
+  const path = [];
+
+  function getPath(root, targetSum) {
+    if (!root) return;
+
+    //   第一次减枝
+    let checkValFlag =
+      root.val <= targetSum ||
+      Math.abs(root.val) ||
+      root.left != null ||
+      root.right != null;
+
+    if (checkValFlag) {
+      path.push(root.val);
+      targetSum -= root.val;
+    } else {
+      return; //无法继续往下搜索
+    }
+    // 2.第二次剪枝
+    if (targetSum === 0 && root.left === null && root.right === null) {
+      result.push([...path]);
+      path.pop();
+      return;
+    }
+    /// 3.第三次剪枝
+    if (root.left !== null) {
+      getPath(root.left, targetSum);
+    }
+
+    /// 3.第四次剪枝
+    if (root.right !== null) {
+      getPath(root.right, targetSum);
+    }
+    path.pop();
   }
-  prevent = prevent.next;
-  slow = slow.next;
-}
-return true;
+  getPath(root, targetSum);
+
+  return result;
+};
+
+// @lc code=end
--- 树/113.路径总和-ii.js
+++ 树/113.路径总和-ii.js
@@ -19,59 +19,74 @@
  * @return {number[][]}
  */
 var pathSum = function (root, targetSum) {
-  // 深度优先
-  //   const arr = [];
-  //   const path = [];
-  //   dfs(root, path, arr, targetSum);
-  //   function dfs(root, path, arr, sum) {
-  //     if (!root) return;
-  //     path.push(root.val);
-  //     if (root.val == sum && root.left == null && root.right == null) {
-  // 满足要求且该节点为叶子节点
-  //       arr.push([...path]);
-  //     }
-  //     dfs(root.left, path, arr, sum - root.val);
-  //     dfs(root.right, path, arr, sum - root.val);
-  //     path.pop();
+  /**
+   * @author xun
+   * @method 深度遍历
+   * @timeComplexity O(N*N)
+   * @spaceComplexity O(N)
+   */
+  // const array = [];
+  // const path = [];
+  // dfs(root, path, array, targetSum);
+  // function dfs(root, path, array, sum) {
+  //   if (!root) return;
+  //   path.push(root.val);
+  //   // 满足要求且该节点为叶子节点
+  //   if (root.val === sum && root.left === null && root.right === null) {
+  //     array.push([...path]);
   //   }
-  //   return arr;
-
+  //   dfs(root.left, path, array, sum - root.val);
+  //   dfs(root.right, path, array, sum - root.val);
+  //   path.pop();
+  // }
+  // return array;
+  /**
+   * @author xun
+   * @method 剪枝+回溯
+   * @timeComplexity O(N)
+   * @spaceComplexity O(N)
+   */
   const result = [];
   const path = [];
 
   function getPath(root, targetSum) {
     if (!root) return;
 
-    //   第一次减枝
-    let checkValFlag =
+    // 第一次剪枝
+    const checkValFlag =
       root.val <= targetSum ||
       Math.abs(root.val) ||
-      root.left != null ||
-      root.right != null;
+      root.left !== null ||
+      root.right !== null;
 
     if (checkValFlag) {
       path.push(root.val);
       targetSum -= root.val;
     } else {
-      return; //无法继续往下搜索
+      //无法继续往下搜索
+      return;
     }
-    // 2.第二次剪枝
-    if (targetSum === 0 && root.left === null && root.right === null) {
+
+    // 第二次剪枝
+    if (targetSum === 0 && root.left === null && root.right == null) {
       result.push([...path]);
       path.pop();
       return;
     }
-    /// 3.第三次剪枝
+
+    // 第三次剪枝
     if (root.left !== null) {
       getPath(root.left, targetSum);
     }
 
-    /// 3.第四次剪枝
+    // 第四次剪枝
     if (root.right !== null) {
       getPath(root.right, targetSum);
     }
+
     path.pop();
   }
+
   getPath(root, targetSum);
 
   return result;
