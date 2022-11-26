21fb89d8d1487f96a364c2af0531d48f4ead40d5
xun <2042204285@qq.com>
2022-11-26 11:05:33 +0800

    moment

---
+++ 栈/71.简化路径.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=71 lang=javascript
+ *
+ * [71] 简化路径
+ */
+
+// @lc code=start
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function (path) {
+  /**
+   * @author xun
+   * @method 栈
+   * @timeComplexity O(N)
+   * @spaceComplexity O(N)
+   */
+  const names = path.split("/");
+  const stack = [];
+  for (const name of names) {
+    if (name == "..") {
+      if (stack.length) stack.pop();
+    } else if (name.length && name !== ".") {
+      stack.push(name);
+    }
+  }
+
+  return "/" + stack.join("/");
+};
+// @lc code=end
---
+++ 栈/84.柱状图中最大的矩形.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=84 lang=javascript
+ *
+ * [84] 柱状图中最大的矩形
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function (heights) {
+  /**
+   * @author xun
+   * @method 单调栈
+   * @timeComplexity O(N)
+   * @spaceComplexity O(N)
+   */
+  let result = 0;
+
+  const stack = [-1];
+  for (let i = 0; i < heights.length; i++) {
+    while (stack.length > 1 && heights[stack[stack.length - 1]] >= heights[i]) {
+      result = Math.max(
+        result,
+        heights[stack.pop()] * (i - stack[stack.length - 1] - 1)
+      );
+    }
+    stack.push(i);
+  }
+
+  while (stack.length > 1) {
+    result = Math.max(
+      result,
+      heights[stack.pop()] * (heights.length - stack[stack.length - 1] - 1)
+    );
+  }
+
+  return result;
+};
+// @lc code=end
