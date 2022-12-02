348d513b97b9f182dba484f988d79d8d87339b52
xun <2042204285@qq.com>
2022-12-02 20:20:05 +0800

    moment

---
+++ 双指针/167.两数之和-ii-输入有序数组.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=167 lang=javascript
+ *
+ * [167] 两数之和 II - 输入有序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  /**
+   * @author xun
+   * @method 双指针
+   * @timeComplexity O(N)
+   * @spaceComplexity O(1)
+   */
+  if (numbers === null) return null;
+
+  let left = 0;
+  let right = numbers.length - 1;
+
+  while (left < right) {
+    let sum = numbers[left] + numbers[right];
+    if (sum === target) return [left + 1, right + 1];
+    else if (sum < target) left++;
+    else right--;
+  }
+
+  return numbers;
+};
+// @lc code=end
