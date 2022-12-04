2affecb7942a26dc734636cddf3019bed5d889cf
xun <2042204285@qq.com>
2022-12-04 18:05:14 +0800

    moment

--- 双指针/4.寻找两个正序数组的中位数.js
+++ 双指针/4.寻找两个正序数组的中位数.js
@@ -11,36 +11,35 @@
  * @return {number}
  */
 var findMedianSortedArrays = function (nums1, nums2) {
-  let num1_length = nums1.length;
-  let num2_length = nums2.length;
-
-  let length = num1_length + num2_length;
+  /**
+   * @author xun
+   * @method 双指针
+   * @timeComplexity O(N)
+   * @spaceComplexity O(1)
+   */
+  const num1_length = nums1.length;
+  const num2_length = nums2.length;
+  const length = num1_length + num2_length;
 
   let previousValue = -1;
   let currentValue = -1;
-
   let point1 = 0;
   let point2 = 0;
 
+  // 双指针直接遍历,向后移动
   // 双指针直接遍历,向后移动
   for (let i = 0; i <= Math.floor(length / 2); i++) {
     previousValue = currentValue;
     if (
       point1 < num1_length &&
       (point2 >= num2_length || nums1[point1] < nums2[point2])
-    ) {
-      currentValue = nums1[point1];
-      point1++;
-    } else {
-      currentValue = nums2[point2];
-      point2++;
-    }
+    )
+      currentValue = nums1[point1++];
+    else currentValue = nums2[point2++];
   }
 
-  if (length % 2 === 0) {
-    return (previousValue + currentValue) / 2;
-  } else {
-    return currentValue;
-  }
+  // 长度为单数的情况
+  if (length % 2 === 0) return (previousValue + currentValue) / 2.0;
+  else return currentValue;
 };
 // @lc code=end
