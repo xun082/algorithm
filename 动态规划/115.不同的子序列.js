1f69228089d0f186a2fdac3f901aab93b4cd5eea
Melody <2042204285@qq.com>
2022-12-19 21:32:27 +0800

    moment

--- 动态规划/115.不同的子序列.js
+++ 动态规划/115.不同的子序列.js
@@ -11,27 +11,30 @@
  * @return {number}
  */
 var numDistinct = function (s, t) {
+  /**
+   * @author xun
+   * @method 动态规划
+   * @timeComplexity O(mn)
+   * @spaceComplexity O(mn)
+   */
   const m = s.length;
   const n = t.length;
 
   if (m < n) return 0;
 
-  const dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+  const path = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
 
   for (let i = 0; i <= m; i++) {
-    dp[i][n] = 1;
+    path[i][n] = 1;
   }
 
   for (let i = m - 1; i >= 0; i--) {
     for (let j = n - 1; j >= 0; j--) {
-      if (s[i] === t[j]) {
-        dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
-      } else {
-        dp[i][j] = dp[i + 1][j];
-      }
+      if (s[i] === t[j]) path[i][j] = path[i + 1][j + 1] + path[i + 1][j];
+      else path[i][j] = path[i + 1][j];
     }
   }
 
-  return dp[0][0];
+  return path[0][0];
 };
 // @lc code=end
