ca2155c8d6e6cb23aa174305ff24c6281f001be9
Melody <2042204285@qq.com>
2022-12-19 21:50:25 +0800

    moment

---
+++ 数学/119.杨辉三角-ii.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=119 lang=javascript
+ *
+ * [119] 杨辉三角 II
+ */
+
+// @lc code=start
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function (rowIndex) {
+  /**
+   * @author xun
+   * @method 线性递推
+   * @timeComplexity O(N)
+   * @spaceComplexity O(1)
+   */
+  const row = new Array(rowIndex + 1).fill(0);
+  row[0] = 1;
+  for (let i = 1; i <= rowIndex; i++) {
+    row[i] = (row[i - 1] * (rowIndex - i + 1)) / i;
+  }
+  return row;
+};
+// @lc code=end
