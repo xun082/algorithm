95283cff81c7bd02ffbd157dc454410fa8018b2f
Melody <2042204285@qq.com>
2022-12-19 21:37:05 +0800

    moment

--- 数学/118.杨辉三角.js
+++ 数学/118.杨辉三角.js
@@ -10,16 +10,21 @@
  * @return {number[][]}
  */
 var generate = function (numRows) {
+  /**
+   * @author xun
+   * @method 数学
+   * @timeComplexity O(N*N)
+   * @spaceComplexity O(1)
+   */
   const result = [];
-
   for (let i = 0; i < numRows; i++) {
     const row = new Array(i + 1).fill(1);
-    for (let j = 1; j < i; j++) {
+    for (let j = 1; j < row.length - 1; j++) {
       row[j] = result[i - 1][j - 1] + result[i - 1][j];
     }
     result.push(row);
   }
-
   return result;
 };
+
 // @lc code=end
