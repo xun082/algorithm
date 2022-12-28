e5054fc560662175510f13fd240570cd910c61a9
Melody <2042204285@qq.com>
2022-12-28 18:38:34 +0800

    moment

--- 栈/151.颠倒字符串中的单词.js
+++ 栈/151.颠倒字符串中的单词.js
@@ -10,29 +10,28 @@
  * @return {string}
  */
 var reverseWords = function (s) {
+  /**
+   * @author xun
+   * @method 栈
+   * @timeComplexity O(N)
+   * @spaceComplexity O(N)
+   */
   const stack = [];
-
   // 是否为单词
   let flag = false;
   let end = s.length;
 
   for (let i = end - 1; i >= 0; i--) {
     // 特殊处理 i===0 && s[0] !==' '这种情况
-    if (i === 0 && s[0] !== " ") {
-      stack.push(s.slice(i, end));
-    } else {
+    if (i === 0 && s[0] !== " ") stack.push(s.slice(i, end));
+    else {
       if (s[i] === " ") {
-        if (flag) {
-          stack.push(s.slice(i + 1, end));
-        }
+        if (flag) stack.push(s.slice(i + 1, end));
         flag = false;
         end = i;
-      } else {
-        flag = true;
-      }
+      } else flag = true;
     }
   }
-
   return stack.join(" ");
 };
 // @lc code=end
