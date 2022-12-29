5651e2b7b6869901c930dc26eac59fc59ea0a6a1
Melody <2042204285@qq.com>
2022-12-29 11:07:59 +0800

    moment

--- 动态规划/213.打家劫舍-ii.js
+++ 动态规划/213.打家劫舍-ii.js
@@ -10,30 +10,29 @@
  * @return {number}
  */
 var rob = function (nums) {
+  /**
+   * @author xun
+   * @method 动态规划
+   * @timeComplexity O(N)
+   * @spaceComplexity O(1)
+   */
   const length = nums.length;
 
-  if (length === 1) {
-    return nums[0];
-  } else if (length === 2) {
-    return Math.max(nums[0], nums[1]);
-  }
-
-  return Math.max(robRange(nums, 0, length - 2), robRange(nums, 1, length - 1));
+  if (length === 1) return nums[0];
+  else if (length === 2) return Math.max(nums[0], nums[1]);
+  return Math.max(range(nums, 0, length - 2), range(nums, 1, length - 1));
 };
 
-const robRange = (nums, start, end) => {
+function range(nums, start, end) {
   let first = nums[start];
-  // 获取到相邻的两个中的最大值
   let second = Math.max(nums[start], nums[start + 1]);
 
-  // 因为偷取相邻的两个会被抓,所以只能头第二个 i+2
   for (let i = start + 2; i <= end; i++) {
     const temp = second;
     second = Math.max(first + nums[i], second);
     first = temp;
   }
-
   return second;
-};
+}
 
 // @lc code=end
