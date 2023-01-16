91f0b07d4046fce2050c6afd60ef7b95aaef5ea9
xun <2042204285@qq.com>
2023-01-16 18:54:24 +0800

    moment

---
+++ 链表/117.填充每个节点的下一个右侧节点指针-ii.js
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=117 lang=javascript
+ *
+ * [117] 填充每个节点的下一个右侧节点指针 II
+ */
+
+// @lc code=start
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function (root) {
+  /**
+   * @author xun
+   * @method 使用已建立的next指针
+   * @timeComplexity O(N)
+   * @spaceComplexity O(1)
+   */
+  if (root === null) return root;
+
+  // 从根节点开始
+  let leftmost = root;
+
+  while (leftmost.left !== null) {
+    // 遍历这一层节点组织成的链表,为下一层的节点更新next 指针
+    let head = leftmost;
+
+    while (head !== null) {
+      head.left.next = head.right;
+
+      if (head.next !== null) {
+        head.right.next = head.next.left;
+      }
+
+      head = head.next;
+    }
+    // 去下一层的最左节点
+    leftmost = leftmost.left;
+  }
+  return root;
+};
+
+// @lc code=end
